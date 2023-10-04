4af30d56bc96d4026535b5e717220b6a52fd3fa8
Victor Grudtner Boell <grudtnerv@outlook.com>
2023-10-04 19:21:09 -0300

    aaaaa

--- dags/teste.py
+++ dags/teste.py
@@ -54,28 +54,28 @@ with models.DAG(
     #                     --zone {CLUSTER_ZONE}",
     # )
 
-    kubernetes_min_pod = GKEStartPodOperator(
-        # The ID specified for the task.
-        task_id="pod-ex-minimum",
-        # Name of task you want to run, used to generate Pod ID.
-        name="pod-ex-minimum",
-        project_id=PROJECT_ID,
-        location=CLUSTER_ZONE,
-        cluster_name=CLUSTER_NAME,
-        # Entrypoint of the container, if not specified the Docker container's
-        # entrypoint is used. The cmds parameter is templated.
-        cmds=["echo"],
-        # The namespace to run within Kubernetes, default namespace is
-        # `default`.
-        namespace="default",
-        # Docker image specified. Defaults to hub.docker.com, but any fully
-        # qualified URLs will point to a custom repository. Supports private
-        # gcr.io images if the Composer Environment is under the same
-        # project-id as the gcr.io images and the service account that Composer
-        # uses has permission to access the Google Container Registry
-        # (the default service account has permission)
-        image="gcr.io/gcp-runtimes/ubuntu_18_0_4",
-    )
+    # kubernetes_min_pod = GKEStartPodOperator(
+    #     # The ID specified for the task.
+    #     task_id="pod-ex-minimum",
+    #     # Name of task you want to run, used to generate Pod ID.
+    #     name="pod-ex-minimum",
+    #     project_id=PROJECT_ID,
+    #     location=CLUSTER_ZONE,
+    #     cluster_name=CLUSTER_NAME,
+    #     # Entrypoint of the container, if not specified the Docker container's
+    #     # entrypoint is used. The cmds parameter is templated.
+    #     cmds=["echo"],
+    #     # The namespace to run within Kubernetes, default namespace is
+    #     # `default`.
+    #     namespace="default",
+    #     # Docker image specified. Defaults to hub.docker.com, but any fully
+    #     # qualified URLs will point to a custom repository. Supports private
+    #     # gcr.io images if the Composer Environment is under the same
+    #     # project-id as the gcr.io images and the service account that Composer
+    #     # uses has permission to access the Google Container Registry
+    #     # (the default service account has permission)
+    #     image="gcr.io/gcp-runtimes/ubuntu_18_0_4",
+    # )
 
     delete_cluster = GKEDeleteClusterOperator(
         task_id="delete_cluster",
