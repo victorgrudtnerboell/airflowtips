69d1f24a8559053e8fc4f6f26c3f6ed981e1c1de
Victor Grudtner Boell <55654512+victorgrudtnerboell@users.noreply.github.com>
2023-10-31 10:27:39 -0300

    Update multiples_branchs.py

--- dags/multiples_branchs.py
+++ dags/multiples_branchs.py
@@ -1,20 +1,10 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from pendulum import datetime, duration
 
 def print_task_type(**kwargs):
     print(f"The {kwargs['task_type']} task has completed.")
-
-def decide_branch(**kwargs):
-    # Determine the branch based on some condition
-    # You can modify this logic as needed
-    if some_condition:  # Replace with your condition
-        return "trigger_son1"
-    elif some_other_condition:  # Replace with another condition
-        return "trigger_son2"
-    else:
-        return "trigger_son3"
 
 # Default settings applied to all tasks
 default_args = {
@@ -26,42 +16,39 @@ default_args = {
     "retry_delay": duration(minutes=5),
 }
 
-with DAG(
-    dag_id="dag_multiples_branchs",
-    start_date=datetime(2023, 1, 1),
-    max_active_runs=1,
-    schedule_interval=None,  # Disable automatic scheduling
-    default_args=default_args,
-    catchup=False,
-) as dag:
-    start_task = PythonOperator(
-        task_id="start_task",
-        python_callable=print_task_type,
-        op_kwargs={"task_type": "starting"},
-    )
+def create_branching_dag(dag_id, condition_task, trigger_son_task, finish_task):
+    with DAG(
+        dag_id=dag_id,
+        start_date=datetime(2023, 1, 1),
+        max_active_runs=1,
+        schedule_interval=None,
+        default_args=default_args,
+        catchup=False,
+    ) as dag:
+        start_task = PythonOperator(
+            task_id=f"{dag_id}_start_task",
+            python_callable=print_task_type,
+            op_kwargs={"task_type": "starting"},
+        )
 
-    decide_branch_task = BranchPythonOperator(
-        task_id="decide_branch",
-        python_callable=decide_branch,
-    )
+        trigger_son = TriggerDagRunOperator(
+            task_id=f"{dag_id}_trigger_son",
+            trigger_dag_id="dag_son",
+            wait_for_completion=True
+        )
 
-    trigger_son1 = TriggerDagRunOperator(
-        task_id="trigger_son1",
-        trigger_dag_id="dag_son1",
-        dag=dag,  # Assign the parent DAG
-    )
+        finish_task = PythonOperator(
+            task_id=f"{dag_id}_finish_task",
+            python_callable=print_task_type,
+            op_kwargs={"task_type": "ending"},
+        )
 
-    trigger_son2 = TriggerDagRunOperator(
-        task_id="trigger_son2",
-        trigger_dag_id="dag_son2",
-        dag=dag,  # Assign the parent DAG
-    )
+        start_task >> condition_task
+        condition_task >> [trigger_son, finish_task]
 
-    trigger_son3 = TriggerDagRunOperator(
-        task_id="trigger_son3",
-        trigger_dag_id="dag_son3",
-        dag=dag,  # Assign the parent DAG
-    )
+    return dag
 
-    start_task >> decide_branch_task
-    decide_branch_task >> [trigger_son1, trigger_son2, trigger_son3]
+# Create three branching DAGs
+dag_1 = create_branching_dag("dag_father_1", condition_task=trigger_son1, trigger_son_task=trigger_son2, finish_task=finish_task)
+dag_2 = create_branching_dag("dag_father_2", condition_task=trigger_son3, trigger_son_task=trigger_son4, finish_task=finish_task)
+dag_3 = create_branching_dag("dag_father_3", condition_task=trigger_son5, trigger_son_task=trigger_son6, finish_task=finish_task)
