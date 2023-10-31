284296088e031e5687acbb75edb8e9bd019a42c2
Victor Grudtner Boell <55654512+victorgrudtnerboell@users.noreply.github.com>
2023-10-31 10:50:07 -0300

    Update multiples_branchs.py

--- dags/multiples_branchs.py
+++ dags/multiples_branchs.py
@@ -1,10 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from pendulum import datetime, duration
-
-def print_task_type(**kwargs):
-    print(f"The {kwargs['task_type']} task has completed.")
 
 # Default settings applied to all tasks
 default_args = {
@@ -16,52 +12,103 @@ default_args = {
     "retry_delay": duration(minutes=5),
 }
 
-def create_branching_dag(dag_id, condition_task, trigger_son_task, finish_task):
-    with DAG(
-        dag_id=dag_id,
-        start_date=datetime(2023, 1, 1),
-        max_active_runs=1,
-        schedule_interval=None,
-        default_args=default_args,
-        catchup=False,
-    ) as dag:
-        start_task = PythonOperator(
-            task_id=f"{dag_id}_start_task",
-            python_callable=print_task_type,
-            op_kwargs={"task_type": "starting"},
-        )
+# Define the son1 DAG
+with DAG(
+    dag_id="son1",
+    start_date=datetime(2023, 1, 1),
+    default_args=default_args,
+    catchup=False,
+) as son1_dag:
 
-        trigger_son = TriggerDagRunOperator(
-            task_id=f"{dag_id}_trigger_son",
-            trigger_dag_id="dag_son",
-            wait_for_completion=True
-        )
+    def son1_task(**kwargs):
+        print("Son1 DAG has been executed successfully.")
 
-        finish_task = PythonOperator(
-            task_id=f"{dag_id}_finish_task",
-            python_callable=print_task_type,
-            op_kwargs={"task_type": "ending"},
-        )
+    son1_task = PythonOperator(
+        task_id="son1_task",
+        python_callable=son1_task,
+    )
 
-        start_task >> condition_task
-        condition_task >> [trigger_son, finish_task]
+# Define the son2 DAG
+with DAG(
+    dag_id="son2",
+    start_date=datetime(2023, 1, 1),
+    default_args=default_args,
+    catchup=False,
+) as son2_dag:
 
-    return dag
+    def son2_task(**kwargs):
+        print("Son2 DAG has been executed successfully.")
 
-# Define your condition, trigger, and finish tasks for each DAG
-condition_task_1 = YourConditionTask1()
-trigger_son_task_1 = YourTriggerSonTask1()
-finish_task_1 = YourFinishTask1()
+    son2_task = PythonOperator(
+        task_id="son2_task",
+        python_callable=son2_task,
+    )
 
-condition_task_2 = YourConditionTask2()
-trigger_son_task_2 = YourTriggerSonTask2()
-finish_task_2 = YourFinishTask2()
+# Define the son3 DAG
+with DAG(
+    dag_id="son3",
+    start_date=datetime(2023, 1, 1),
+    default_args=default_args,
+    catchup=False,
+) as son3_dag:
 
-condition_task_3 = YourConditionTask3()
-trigger_son_task_3 = YourTriggerSonTask3()
-finish_task_3 = YourFinishTask3()
+    def son3_task(**kwargs):
+        print("Son3 DAG has been executed successfully.")
 
-# Create three branching DAGs
-dag_1 = create_branching_dag("dag_father_1", condition_task_1, trigger_son_task_1, finish_task_1)
-dag_2 = create_branching_dag("dag_father_2", condition_task_2, trigger_son_task_2, finish_task_2)
-dag_3 = create_branching_dag("dag_father_3", condition_task_3, trigger_son_task_3, finish_task_3)
+    son3_task = PythonOperator(
+        task_id="son3_task",
+        python_callable=son3_task,
+    )
+
+# Create the father DAG
+with DAG(
+    dag_id="father_dagaaa",
+    start_date=datetime(2023, 1, 1),
+    default_args=default_args,
+    catchup=False,
+) as father_dag:
+
+    def print_start(**kwargs):
+        print("Starting the workflow.")
+
+    start_task = PythonOperator(
+        task_id="start_task",
+        python_callable=print_start,
+    )
+
+    def always_true(**kwargs):
+        return "son1_task"
+
+    branch_task_son1 = PythonOperator(
+        task_id="branch_task_son1",
+        python_callable=always_true,
+    )
+
+    def always_true(**kwargs):
+        return "son2_task"
+
+    branch_task_son2 = PythonOperator(
+        task_id="branch_task_son2",
+        python_callable=always_true,
+    )
+
+    def print_terminate(**kwargs):
+        print("Workflow terminated.")
+        return "terminate_workflow"
+
+    terminate_workflow = PythonOperator(
+        task_id="terminate_workflow",
+        python_callable=print_terminate,
+    )
+
+    # Set up the order of tasks in the father DAG
+    start_task >> branch_task_son1
+    branch_task_son1 >> son1_dag
+    branch_task_son1 >> terminate_workflow
+
+    son1_dag >> branch_task_son2
+    branch_task_son2 >> son2_dag
+    branch_task_son2 >> terminate_workflow
+
+    son2_dag >> son3_dag
+    son3_dag >> terminate_workflow
